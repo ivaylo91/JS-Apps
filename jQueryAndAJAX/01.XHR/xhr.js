427ae2143882ccaf31b4362f03e926d6d1816552
ivaylo91 <ipenev91@gmail.com>
2018-01-30 15:59:17 +0200

    Print All repos in my github profile.

--- jQueryAndAJAX/01.XHR/xhr.js
+++ jQueryAndAJAX/01.XHR/xhr.js
@@ -1,6 +1,10 @@
 function loadRepos() {
     let req = new XMLHttpRequest();
     req.addEventListener('load', function () {
+        if (req.status === 404) {
+            document.getElementById('result').textContent = "Error 404 - Not Found";
+            return;
+        }
         let repos = JSON.parse(req.responseText);
         let output = document.getElementById('result');
         output.innerHTML = '<ul>';
@@ -9,6 +13,6 @@ function loadRepos() {
         }
         output.innerHTML += '</ul>';
     });
-    req.open("GET", "https://api.github.com/users/ivaylo91/repos", true);
+    req.open("GET", "https://api.github.com/usersers/ivaylo91/repos", true);
     req.send();
 }
