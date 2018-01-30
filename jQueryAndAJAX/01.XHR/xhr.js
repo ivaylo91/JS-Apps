387b8748614175a8ca4af89171d0623aec70b409
ivaylo91 <ipenev91@gmail.com>
2018-01-30 15:41:40 +0200

    Print All repos in my github profile.

--- jQueryAndAJAX/01.XHR/xhr.js
+++ jQueryAndAJAX/01.XHR/xhr.js
@@ -1,16 +1,14 @@
 function loadRepos() {
     let req = new XMLHttpRequest();
-    req.onreadystatechange = function () {
-        if (req.readyState === 4 && req.status === 200) {
-            let repos = JSON.parse(req.responseText);
-            let output = document.getElementById('result');
-            output.innerHTML = '<ul>';
-            for (let repo  of repos) {
-                output.innerHTML += `<li>${JSON.stringify(repo.name)}</li>`;
-            }
-            output.innerHTML+= '</ul>';
+    req.addEventListener('load', function () {
+        let repos = JSON.parse(req.responseText);
+        let output = document.getElementById('result');
+        output.innerHTML = '<ul>';
+        for (let repo  of repos) {
+            output.innerHTML += `<li>${JSON.stringify(repo.name)}</li>`;
         }
-    };
+        output.innerHTML += '</ul>';
+    });
     req.open("GET", "https://api.github.com/users/ivaylo91/repos", true);
     req.send();
 }
