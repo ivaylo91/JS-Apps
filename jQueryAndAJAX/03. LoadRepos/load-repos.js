de61cd0907060a98610e90836b439a25c4a31ddc
ivaylo91 <ipenev91@gmail.com>
2018-01-31 15:45:26 +0200

    Load Repos jQuery AJAX different users

--- jQueryAndAJAX/03. LoadRepos/load-repos.js
+++ jQueryAndAJAX/03. LoadRepos/load-repos.js
@@ -13,7 +13,7 @@ function display(repos) {
     let output = $('#result');
     let list = $('<ul>');
     for (let repo  of repos) {
-        $(`<li>${(repo.name)}</li>`).appendTo(list);
+        $(`<li><a href="${repo.html_url}" target="_blank"> ${(repo.name)}</a></li>`).appendTo(list);
     }
     output.append(list);
 }
